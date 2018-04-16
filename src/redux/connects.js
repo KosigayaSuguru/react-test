24f8b9e2ea11c6fdc79d9f9150ebc7e1d5b5de2d
KosigayaSuguru <asd7220017@gmail.com>
2018-04-17 00:46:04 +0900

    mapDispatchToPropsのコメント追加

--- src/redux/connects.js
+++ src/redux/connects.js
@@ -6,6 +6,11 @@ export function mapStateToProps(state) {
     value: state.value
   };
 }
+
+// returnするメソッドがreduxと連携したコンポーネントのpropsに渡される
+// 下記の例でいうと、reduxと連携したコンポーネントから、
+// this.pops.countup, etc
+// が出来るようになる
 export function mapDispatchToProps(dispatch) {
   return {
     countup(value) {
